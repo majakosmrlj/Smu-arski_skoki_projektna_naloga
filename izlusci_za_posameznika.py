0e9f1901d440f56977174bc219aa8707f63b461f
majakosmrlj <mk37033@student.uni-lj.si>
2024-08-20 22:53:33 +0200

    manjse spremembe kode

--- izlusci_za_posameznika.py
+++ izlusci_za_posameznika.py
@@ -4,8 +4,10 @@ import json
 import html
 from collections import OrderedDict
 
+#za kodo ne deluje vse pravilo, zdi se mi da je vzorec nekaj narobe
 
 vzorec_vseh_podatkov = re.compile(
+    #r'<div style="background-image:url\(\'https://data\.fis-ski\.com/general/load-competitor-picture/(?P<koda>.*?)\.html\'\);" class="avatar__image"></div>.*?'
     r'<h1 class="athlete-profile__name">(?P<ime>.*?)<span class="athlete-profile__lastname">(?P<priimek>.*?)</span>.*?'
     r'<div class="athlete-profile__team spacer__section">(?P<klub>.*?)</div>.*?'
     r'<span class="country__name">(?P<drzava>.*?)</span>.*?'
@@ -13,10 +15,6 @@ vzorec_vseh_podatkov = re.compile(
     r'<li class="profile-info__entry profile-info__entry_visible_xs" id="FIS Code">\s*?'
     r'<span class="profile-info__field">FIS Code</span>\s*?'
     r'<span class="profile-info__value">(?P<id>.*?)</span>.*?'
-    # #za pridobitev rojstnega dneva
-    # # r'<li class="profile-info__entry profile-info__entry_visible_xs" id="Birthdate">\s+?'
-    # # r'<span class="profile-info__field">Birthdate</span>\s*?'
-    # # r'<span class="profile-info__value">(?P<rojstni_dan>.*?)</span>.*?'
     #za trenutni status
     r'<li class="profile-info__entry profile-info__entry_visible_xs" id="Status">\s*?'
     r'<span class="profile-info__field">Status</span>\s*?'
@@ -35,7 +33,7 @@ vzorec_vseh_podatkov = re.compile(
     r'<span class="profile-info__value">(?P<smuci>.+?)</span>',
     flags = re.DOTALL
 )
-#za rd je lahko &nbsp;___PAZI
+
 
 
 vzorec_rojstnega_leta = re.compile(
@@ -47,9 +45,10 @@ vzorec_rojstnega_leta = re.compile(
 
 def izlusci_posameznega_smucarja(vsebina):
     smucar = vzorec_vseh_podatkov.search(vsebina).groupdict()
+    #smucar["koda"] = int(smucar['koda'])
     smucar['ime'] = smucar["ime"].strip()
     smucar['priimek'] = smucar["priimek"].strip().capitalize()
-    #za klub, da če ni podatka vrne None
+    #za klub
     if smucar["klub"] == '':
         smucar['klub'] = None
     else:
@@ -122,86 +121,3 @@ def izlusci_posameznega_smucarja(vsebina):
 
 # izlusci_smucarja(koda)
 
-
-
-#pazi, da je isti vrstni red
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#ZA POMOC, OD PROFESORJA
-#     igralci = []
-#     for najdba in igralci_re.finditer(besedilo):
-#         igralci.append((najdba["id"], najdba["ime"]))
-#     if len(igralci) == 0:
-#         print("napaka: igralci", id)
-
-#     # izluscimo leto, oznako in cas
-#     lastosti_re = re.compile(
-#         r'href="/title/tt\d+/releaseinfo\?ref_=tt_ov_rdat">(\d+)</a></li>'
-#         r"(.*?)"
-#         r'<li role="presentation" class="ipc-inline-list__item">((\d+?h ?)?(\d+?m)?)</li>'
-#         )
-#     najdba = lastosti_re.search(besedilo)
-#     leto = ""
-#     oznaka = ""
-#     cas = ""
-#     if najdba is not None:
-#         leto = najdba.group(1)
-#         oznaka_del = najdba.group(2)
-#         oznaka_re = re.compile(r'<li role="presentation".+?tt_.+?">(.+?)</a></li>')
-#         najdba2 = oznaka_re.search(oznaka_del)
-#         if najdba2 is not None:
-#             oznaka = najdba2.group(1)
-#         cas_tekst = najdba.group(3)
-#         # spremenimo cas v minute
-#         cas = 0
-#         ure_re = re.compile(r"(\d*)h")
-#         najdba3 = ure_re.search(cas_tekst)
-#         if najdba3 is not None:
-#             cas = 60 * int(najdba3.group(1))
-#         minute_re = re.compile(r"(\d*)m")
-#         najdba3 = minute_re.search(cas_tekst)
-#         if najdba3 is not None:
-#             cas += int(najdba3.group(1))
-#  else:
-#         print("napaka: lastnosti", id)
-
-#     # izluscimo reziserje
-#     reziserji = []
-#     reziserji_re = re.compile(r'directors?":\[(.+?)\]')
-#     najdba = reziserji_re.search(besedilo)
-#     if najdba is not None:
-#         reziser_re = re.compile(
-#             r'{"@type":"Person","url":"https://www.imdb.com/name/nm(\d+)/","name":"(.+?)"}'
-#         )
-#         for najdba2 in reziser_re.finditer(najdba.group(1)):
-#             id_osebe = najdba2.group(1)
-#             ime = najdba2.group(2)
-#             reziserji.append((id_osebe, ime))
-#         if len(reziserji) == 0:
-#             print("napaka: reziserji", id)
-#     else:
-#         print("napaka: reziserji", id)
-
-#     # izluscimo oceno
-#     rating_re = re.compile(r'__aggregate-rating__score".+?>(\d.\d)</span>')
-#     najdba = rating_re.search(besedilo)
-#     if najdba is not None:
-#         ocena = najdba.group(1)
-#     else:
-#         print("napaka: ocena", id)
-    
-#         return (igralci, reziserji, ocena, cas, leto, oznaka)
