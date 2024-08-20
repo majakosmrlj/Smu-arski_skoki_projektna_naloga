72e1733ee25dd82271241bbd5d7c83768f256a5c
majakosmrlj <mk37033@student.uni-lj.si>
2024-08-20 22:00:46 +0200

    dodala zanko s katero se sprehodimo cez posamezne strani in izluščimo podatke

--- pridobi_vse_podatke.py
+++ pridobi_vse_podatke.py
@@ -4,6 +4,7 @@ import time
 import re
 
 #da sem dobila dol vseh 9 strani s smucarskimi skakalci
+
 # for i in range(9):
 #     url = f"https://www.fis-ski.com/DB/ski-jumping/biographies.html?lastname=&firstname=&sectorcode=JP&gendercode=&birthyear=&skiclub=&skis=&nationcode=&fiscode=&status=&search=true&limit=1000&offset={i * 1000}"
 #     odziv = requests.get(url)
@@ -23,25 +24,40 @@ import re
 #preverimo koliko je vseh smucarskih skakalcev in  dobimo ven njihove kode
 kode = []
 for i in range(9):
-    with open(f"smucarji_stran_{i}.html") as d:
+    with open(f"smucarji/smucarji_stran_{i}.html") as d:
         vsebina = d.read()
         niz_za_kodo = r'<a class="table-row" href="https://www\.fis-ski\.com/DB/general/athlete-biography\.html\?sectorcode=JP&amp;competitorid=(?P<koda>\d+)" target="_self">'
         for najdba in re.finditer(niz_za_kodo, vsebina):
             kode.append(int(najdba["koda"]))
-    print (len(kode))
+print (len(kode))
+
+#shranjevanje vseh spletnih strani posameznih smucarskih skakalcev
+
+# count = 0
+# for koda in kode:
+#     url = f"https://www.fis-ski.com/DB/general/athlete-biography.html?sectorcode=JP&competitorid={koda}&type=result&categorycode=&sort=&place=&disciplinecode=&position=&limit=1000"
+#     odgovor = requests.get(url)    
+#     if odgovor.status_code == 200:
+#         count += 1
+#         with open(os.path.join("posamezni_smucarji", f"smucar_{koda}.html"), "w", encoding ="utf8") as dat:
+#             dat.write(odgovor.text)
+#         time.sleep(1)
+#     else:
+#         print("url")
+#         break
+
+# print(count)            
+
+
+from izlusci_za_posameznika import *
+
 
 
 count = 0
-for koda in kode:
-    url = f"https://www.fis-ski.com/DB/general/athlete-biography.html?sectorcode=JP&competitorid={koda}&type=result&categorycode=&sort=&place=&disciplinecode=&position=&limit=1000"
-    odgovor = requests.get(url)    
-    if odgovor.status_code == 200:
-        count += 1
-        with open(os.path.join("posamezni_smucarji", f"smucar_{koda}.html"), "w", encoding ="utf8") as dat:
-            dat.write(odgovor.text)
-        time.sleep(1)
-    else:
-        print("url")
-        break
-
-print(count)            
+for koda in kode[:100]:
+    with open(f"smucarji/posamezni_smucarji/smucar_{koda}.html", encoding ="utf8") as dat:
+        vsebina = dat.read()
+        for vzorec in vzorec_vseh_podatkov.finditer(vsebina):
+            print(izlusci_posameznega_smucarja(vsebina))
+            count += 1
+print(count)
